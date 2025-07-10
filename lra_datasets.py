ecd3ef7d36266d8e83c8b518cd360df54a0e2ae0
Charles Dea <charliedea@proton.me>
2025-07-10 15:24:16 -0500

    added print statement

--- lra_datasets.py
+++ lra_datasets.py
@@ -215,6 +215,7 @@ class Pathfinder256Dataset:
         self.split=split
     def __getitem__(self,i):
         f=i%1000
+        print(i,f)
         fn='lra_release/lra_release/pathfinder256/curv_baseline/imgs/'+str(f+200*(.85 if self.split=='eval'else 0))+'/sample_'+str(i-f*1000)+'.png'
         return self.tokenizer(Image.open(fn).convert('L'),self.max_length),torch.LongTensor([self.lbls[i]])
         #return self.tokenizer(self.data[i],self.max_length),torch.LongTensor([self.lbls[i]])
