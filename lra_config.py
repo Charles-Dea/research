0620617d5eb28eb3e8faf7e32f918cdb1284aeb3
Charles Dea <charliedea@proton.me>
2025-07-21 15:36:00 -0500

    changed learning rate

--- lra_config.py
+++ lra_config.py
@@ -168,8 +168,8 @@ def get_pathfinder32_config():
     config.total_eval_samples=tsts
     eps=2000
     config.total_train_samples=trns*eps
-    config.weight_decay=10
-    config.learning_rate=.05
+    config.weight_decay=0
+    config.learning_rate=1
     config.warmup_steps=8000
     config.tied_weights=False
     config.max_length=1024
